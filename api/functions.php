d2427cddbfd6e267bd34c8407406467e2e8d237a
The Sick Dude <jj.strootman@alfa-college.nl>
2019-09-18 13:49:57 +0200

    Upload van de les van 18-09-2019

---
+++ api/functions.php
@@ -0,0 +1,87 @@
+<?php
+
+@include_once('db.php');
+
+$status_messages = [
+    200 => 'Ok',
+    201 => 'Created',
+    202 => 'Accepted',
+    400 => 'Bad request',
+    401 => 'Unauthorized',
+    402 => 'Payment required',
+    403 => 'Forbidden',
+    404 => 'Not found',
+    500 => 'Internal Server Error',
+    501 => 'Not implemented',
+    502 => 'Bad Gateway',
+    503 => 'Service Unavailable'
+];
+
+$content_types = [
+    'json' => 'application/json',
+    'xml' => 'application/xml',
+    'zip' => 'application/zip',
+    'html' => 'text/html',
+    'pdf' => 'application/pdf',
+    'css' => 'text/css',
+    'csv' => 'text/csv',
+    'txt' => 'text/plain',
+    'png' => 'image/png',
+    'jpg' => 'image/jpeg',
+    'gif' => 'image/gif'
+];
+
+function responseHeader($code, $content_type = 'json')
+{
+    global $status_messages, $content_types;
+
+    header("HTTP/1.1 $code {$status_messages[$code]}");
+    header("Content-Type: {$content_types[$content_type]}");
+}
+
+function errorResponse($msg, $code)
+{
+    responseHeader($code, 'json');
+
+    echo json_encode([
+        'status' => $code,
+        'error_message' => $msg
+    ]);
+
+    exit(0);
+}
+
+function sendResponse($data)
+{
+    responseHeader(200, 'json');
+
+    echo json_encode($data);
+    exit(0);
+}
+
+function getValueOfCurrency($cur)
+{
+    $currency = strtoupper($cur);
+
+    if(dbConnect()) {
+        $sql = "SELECT * FROM currency WHERE abbr = :abbr";
+        $params = [ ':abbr' => $currency ];
+        dbQuery($sql, $params);
+
+        // @TODO: Error response voor het geval de valuta code niet gevonden is
+
+        return dbGet();
+    }
+
+    errorResponse('Connection to database impossible', 500);
+}
+
+function getCalculatedValueInEuros($cur, $amount)
+{
+
+}
+
+function getCalculatedValue($cur, $amount, $tocur)
+{
+
+}
---
+++ api/index.php
@@ -0,0 +1,40 @@
+<?php
+
+@include_once('functions.php');
+
+
+
+if(!isset($_GET['cmd'])) {
+    errorResponse('You did not use this API the right way', 400);
+}
+
+$data = [];
+
+switch(strtolower($_GET['cmd'])) {
+    case 'value':
+        if(isset($_GET['cur']))
+            $data = getValueOfCurrency($_GET['cur']);
+        break;
+
+    case 'calc':
+        if(isset($_GET['cur']) && isset($_GET['amount']))
+            $data = getCalculatedValueInEuros($_GET['cur'], $_GET['amount']);
+        break;
+
+    case 'calcval':
+        if(isset($_GET['cur']) && isset($_GET['amount']) && isset($_GET['tocur']))
+            $data = getCalculatedValue($_GET['cur'], $_GET['amount'], $_GET['tocur']);
+        break;
+
+    default:
+        errorResponse('Command unknown', 404);
+}
+
+sendResponse($data);
+
+
+
+
+
+
+
